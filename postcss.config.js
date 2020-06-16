871d7fd96337c06c4df48e6aa0212db1359a56b0
Iván Munguia <ivanalejandro249@gmail.com>
2020-06-16 18:50:44 -0500

    Configure postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,22 @@
 module.exports = {
   plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("postcss-import"),
+    require("postcss-import")({
+      from: "app/javascript/stylesheets/application.css",
+    }),
     require("postcss-flexbugs-fixes"),
     require("postcss-preset-env")({
       autoprefixer: {
         flexbox: "no-2009",
       },
-      stage: 3,
+      stage: 1,
+      features: {
+        customProperties: {
+          warnings: false,
+          preserve: true,
+        },
+      },
     }),
+    require("tailwindcss"),
+    require("autoprefixer"),
   ],
 };
