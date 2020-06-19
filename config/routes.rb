cd68efa63fc7c89d63d26aaf36a2a15f80ed92d2
Iván Munguia <ivanalejandro249@gmail.com>
2020-06-19 13:57:39 -0500

    Configure Graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    post "/graphql", to: "api/graphql#execute"
   end
 
   devise_for :users
