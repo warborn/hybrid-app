a2d6b3c2ba45cf6cf0dbc93b1e89ba4758eeb15a
Iván Munguia <ivanalejandro249@gmail.com>
2020-06-19 17:49:52 -0500

    Create GraphQL endpoint to return current user data

--- app/controllers/api/graphql_controller.rb
+++ app/controllers/api/graphql_controller.rb
@@ -4,14 +4,19 @@ module Api
     # This allows for outside API access while preventing CSRF attacks,
     # but you'll have to authenticate your user separately
     # protect_from_forgery with: :null_session
+    before_action :authenticate_api_user!
+    before_action :set_current_user
+
+    def set_current_user
+      @current_user ||= warden.authenticate(scope: :api_user)
+    end
 
     def execute
       variables = ensure_hash(params[:variables])
       query = params[:query]
       operation_name = params[:operationName]
       context = {
-        # Query context goes here, for example:
-        # current_user: current_user,
+        current_user: @current_user,
       }
       result = HyrbridAppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
       render json: result
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,10 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :me, Types::UserType, null: false,
+      description: "The data of the current user"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def me
+      context[:current_user]
     end
   end
 end
